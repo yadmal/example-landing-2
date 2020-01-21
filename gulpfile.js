2cd38c2ca35a2f9efae38cfb9c2171d0d361eda0
YashkinDmitry <yashkin23@gmail.com>
2020-01-21 16:32:18 +0300

    Add module gulp-clean-css in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
+const cleanCss = require('gulp-clean-css');
 
 function styles(){
   return gulp.src('./src/preCss/**/*.css')
@@ -9,6 +10,7 @@ function styles(){
     overrideBrowserslist:['>0.1%'],
     cascade:false
   }))
+  .pipe(cleanCss({level:2}))
   .pipe(gulp.dest('./build/css'));
 }
 
