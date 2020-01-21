87a01fff6ad654df66695d4f7f972297cd196900
YashkinDmitry <yashkin23@gmail.com>
2020-01-21 16:39:03 +0300

    Add module gulp-uglify in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCss = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
 
 function styles(){
   return gulp.src('./src/preCss/**/*.css')
@@ -16,6 +17,8 @@ function styles(){
 
 function scripts(){
   return gulp.src('./src/preJs/**/*.js')
+  .pipe(concat('script.css'))
+  .pipe(uglify({toplevel:true}))
   .pipe(gulp.dest('./build/js'));
 }
 
