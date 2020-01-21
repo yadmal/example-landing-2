8fe977b7fe50403f7f962f8f8b2b7c17da5c880a
YashkinDmitry <yashkin23@gmail.com>
2020-01-21 16:55:43 +0300

    Add module gulp-browser-sync in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCss = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
+const browserSync = require('browser-sync').create();
 
 function styles(){
   return gulp.src('./src/preCss/**/*.css')
@@ -12,21 +13,32 @@ function styles(){
     cascade:false
   }))
   .pipe(cleanCss({level:2}))
-  .pipe(gulp.dest('./build/css'));
+  .pipe(gulp.dest('./build/css'))
+  .pipe(browserSync.stream());
+
 }
 
 function scripts(){
   return gulp.src('./src/preJs/**/*.js')
   .pipe(concat('script.css'))
   .pipe(uglify({toplevel:true}))
-  .pipe(gulp.dest('./build/js'));
+  .pipe(gulp.dest('./build/js'))
+  .pipe(browserSync.stream());
 }
 
 function watch(){
+  browserSync.init({
+    server: {
+      basedir: "./"
+    }
+  });
   gulp.watch('./src/preCss/**/*.css', styles);
   gulp.watch('./src/preJs/**/*.js', scripts);
+  gulp.watch('./*.html').on('change', browserSync.reload);
 }
 
 gulp.task('styles', styles);
 gulp.task('scripts', scripts);
 gulp.task('watch', watch);
+gulp.task('build', gulp.series(gulp.parallel(styles, scripts)));
+gulp.task('dev', gulp.series('build', 'watch'));
